e0e22ed184a22feffab6716b3b5934d24bea9597
Luis Manzo <manzosk3@gmail.com>
2021-03-24 16:25:18 -0400

    adds y-offset and first char highlights

--- sketch.js
+++ sketch.js
@@ -1,16 +1,16 @@
 var charSize = 20;
 var streams = [];
 
-function setup() {  
+function setup() {
   createCanvas(
     window.innerWidth,
     window.innerHeight
   )
-  background(0);
+  background(0, 50);
   var x = 0;
-  for(var i = 0; i <= width / charSize; i++) {
+  for (var i = 0; i <= width / charSize; i++) {
     stream = new Stream();
-    stream.generateChars(x, random(-500, 0));
+    stream.generateChars(x, random(innerHeight, 0));
     streams.push(stream);
     x += charSize;
   }
@@ -19,18 +19,19 @@ function setup() {
 
 function draw() {
   background(0);
-  streams.forEach(function(stream) {
+  streams.forEach(function (stream) {
     stream.render();
   });
 }
 
 
-function Character(x, y, speed) {
+function Character(x, y, speed, first) {
   this.x = x;
   this.y = y;
   this.value;
   this.speed = speed;
   this.switchInterval = round(random(2, 20));
+  this.first = first;
 
   this.setToRandomChar = function () {
     if (frameCount % this.switchInterval === 0) {
@@ -56,16 +57,22 @@ function Stream() {
   this.totalChars = round(random(5, 20));
   this.speed = random(1, 5);
   this.generateChars = function (x, y) {
+    var first = true;
     for (var i = 0; i <= this.totalChars; i++) {
-      char = new Character(x, y, this.speed);
+      char = new Character(x, y, this.speed, first);
       char.setToRandomChar();
       this.chars.push(char);
       y -= charSize;
+      first = false;
     };
   }
   this.render = function () {
     this.chars.forEach(function (char) {
-      fill(0, 255, 70);
+      if (char.first) {
+        fill(255, 255, 180)
+      } else {
+        fill(0, 255, 70)
+      }
       text(char.value, char.x, char.y);
       char.rain();
       char.setToRandomChar();
