df68588faa7018ad5caba0fa406b2b55851c312c
Luis Manzo <manzosk3@gmail.com>
2021-03-24 15:44:29 -0400

    sets stream to render characters

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
-var char;
-var symbolSize = 60;
+var charSize = 20;
 
 function setup() {
   createCanvas(
@@ -7,18 +6,14 @@ function setup() {
     window.innerHeight
   )
   background(0);
-  char = new Character(
-    width / 2,
-    0,
-    random(1, 5)
-  );
-  char.setToRandomSymbol();
-  textSize(symbolSize);
+  stream = new Stream();
+  stream.generateChars();
+  textSize(charSize);
 }
 
 function draw() {
   background(0);
-  char.render();
+  stream.render();
 }
 
 
@@ -27,22 +22,19 @@ function Character(x, y, speed) {
   this.y = y;
   this.value;
   this.speed = speed;
+  this.switchInterval = round(random(2, 20));
 
-  this.setToRandomSymbol = function () {
-    this.value = String.fromCharCode(
-      0x30A0 + round(random(0, 96))
-    );
+  this.setToRandomChar = function () {
+    if (frameCount % this.switchInterval === 0) {
+      this.value = String.fromCharCode(
+        0x4E00 + round(random(0, 96))
+      );
+    }
+
   };
 
-  this.render = function () {
-    fill(0, 255, 70);
-    text(this.value, this.x, this.y);
-    this.rain();
-    this.setToRandomSymbol();
-  };
-
-  this.rain = function() {
-    if(this.y >= height) {
+  this.rain = function () {
+    if (this.y >= height) {
       this.y = 0;
     } else {
       this.y += this.speed;
@@ -51,5 +43,26 @@ function Character(x, y, speed) {
   }
 }
 
-
-
+function Stream() {
+  this.chars = [];
+  this.totalChars = round(random(5, 30));
+  this.speed = random(1, 5);
+  this.generateChars = function () {
+    var y = 0;
+    var x = width / 2;
+    for (var i = 0; i <= this.totalChars; i++) {
+      char = new Character(x, y, this.speed);
+      char.setToRandomChar();
+      this.chars.push(char);
+      y -= charSize;
+    };
+  }
+  this.render = function () {
+    this.chars.forEach(function (char) {
+      fill(0, 255, 70);
+      text(char.value, char.x, char.y);
+      char.rain();
+      char.setToRandomChar();
+    })
+  }
+}
